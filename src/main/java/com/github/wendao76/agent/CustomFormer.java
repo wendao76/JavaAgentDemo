850903ddcf2effbc714b7b5f2bcc1979706fb071
李文虎 <liwenhu@cyou-inc.com>
2020-01-14 18:32:58 +0800

    代理类bugfix

--- src/main/java/com/github/wendao76/agent/CustomFormer.java
+++ src/main/java/com/github/wendao76/agent/CustomFormer.java
@@ -6,40 +6,48 @@ import javassist.CtBehavior;
 import javassist.CtClass;
 
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.security.ProtectionDomain;
 
+/**
+ * @author wendao76
+ */
 public class CustomFormer implements ClassFileTransformer {
+    public final String injectedClassName = "com.github.wendao76";
+
     @Override
     public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
+        className = className.replace("/", ".");
         byte[] transformed = null;
-        System.out.println("Transforming " + className);
         ClassPool classPool = ClassPool.getDefault();
         CtClass cl = null;
-
-        try {
-            cl = classPool.makeClass(new ByteArrayInputStream(classfileBuffer));
-            if(cl.isInterface() == false) {
-                CtBehavior[] methods = cl.getDeclaredBehaviors();
-                for (int i=0; i<methods.length; i++) {
-                    doMethod(methods[i]);
+        if(className.startsWith(injectedClassName)) {
+            System.out.println("Transforming " + className);
+            try {
+                cl = classPool.makeClass(new ByteArrayInputStream(classfileBuffer));
+                if(!cl.isInterface()) {
+                    CtBehavior[] methods = cl.getDeclaredBehaviors();
+                    for (int i=0; i<methods.length; i++) {
+                        doMethod(methods[i]);
+                    }
+                    transformed = cl.toBytecode();
                 }
-                transformed = cl.toBytecode();
+                return transformed;
+            } catch (Exception e) {
+                e.printStackTrace();
+//                System.out.println("Could not instrument  " + className + ", exception:" + e.getMessage());
+            } finally {
+                if(cl != null) {
+                    cl.detach();
+                }
             }
-        } catch (IOException e) {
-            System.out.println("Could not instrument  " + className + ", exception:" + e.getMessage());
-        } catch (CannotCompileException e) {
-           if(cl != null) {
-               cl.detach();
-           }
         }
-        return transformed;
+        return null;
     }
 
     private void doMethod(CtBehavior method) throws CannotCompileException {
-        method.insertBefore("long stime = System.nanoTime();");
-        method.insertAfter("System.out.Printlon(\" leave" + method.getName() + "and time: \" +(System.nanoTime()-stime))");
+        method.insertBefore("System.out.println(\"before:\"+\"" +method.getName()+"\");");
+        method.insertAfter("System.out.println(\"after:\"+\""+method.getName()+"\");");
     }
 }
